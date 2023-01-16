c2b3b79988a07887de1206f83619478325e218b3
İsmail Keskin <hamdismailkskn98@gmail.com>
2023-01-16 20:36:53 +0300

    selectors tanımlandı ve tasarım düzeltildi

---
+++ javascript-applications/form-validator/script.js
@@ -0,0 +1,6 @@
+//! Selectors
+const form = document.querySelector("#form");
+const userName = document.querySelector("#username");
+const eMail = document.querySelector("#email");
+const password = document.querySelector("#password");
+const rePassword = document.querySelector("#repassword");
