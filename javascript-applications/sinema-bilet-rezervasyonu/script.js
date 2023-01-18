baa47c87dfb5c03c71a8776d5ec1867589f59158
İsmail Keskin <hamdismailkskn98@gmail.com>
2023-01-18 22:50:16 +0300

    Bilet bilgilerinin local storage'den alınması ve proje tamamlandı

--- javascript-applications/sinema-bilet-rezervasyonu/script.js
+++ javascript-applications/sinema-bilet-rezervasyonu/script.js
@@ -11,6 +11,12 @@ const amount = document.querySelector("#amount");
 const seats = document.querySelectorAll(".seat:not(.reserved)");
 // seçilen koltuklar
 let selectedSeats;
+
+// local strage'den verileri çekme
+getLocalStorage();
+// toplam fiyatlama
+calculateTotal();
+
 //! Events
 
 // koltukları seçme
@@ -51,28 +57,28 @@ function calculateTotal() {
     // ücret hesaplaması
     let price = select.value * selectedSeatCount;
     amount.innerText = price;
-    
+
 }
 
 // seçilen ve seçilmeyen koltukları array haline getirme
-function seatsArray(){
+function seatsArray() {
     // seçilen koltuklar için önce boş dizi
     selectedSeatsArr = [];
     // boş koltuklar için önce boş dizi
     seatsArr = [];
     // seçilen koltukların üzerinde dolaş her birini seat'ın içine koy
     // "selectedSeatsArr" in içine push et
-    for(let seat of selectedSeats){
+    for (let seat of selectedSeats) {
         selectedSeatsArr.push(seat);
     }
     // "seats" nodeList'in üzerinde dolaş her birini seat'ın içine koy
     //"seatsArr'"in içine push et
-    for(let seat of seats){
+    for (let seat of seats) {
         seatsArr.push(seat);
     }
     // seçilen koltukların index numaralarını bulma
     // bulunan index numaraları array halinde geri döner
-    let selectSeatIndexs = selectedSeatsArr.map(function(seat){
+    let selectSeatIndexs = selectedSeatsArr.map(function (seat) {
         return seatsArr.indexOf(seat);
     });
 
@@ -83,9 +89,25 @@ function seatsArray(){
 //! Local Storage
 
 // oluşturduğumuz array'leri local storage'e gönderme
-function setLocalStorage(indexs){
+function setLocalStorage(indexs) {
     // local storage'e seçilen koltukların index'lerini array halinde ekleme
-    localStorage.setItem("selectedSeats",JSON.stringify(indexs));
+    localStorage.setItem("selectedSeats", JSON.stringify(indexs));
     // seçili olan select (film) dinamik olarak eklenmekte
     localStorage.setItem("selectedMovieIndex", select.selectedIndex);
+}
+
+//local storage'den verileri çekme
+function getLocalStorage() {
+    let selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+    if(!(selectedSeats == null) && selectedSeats.length > 0){
+        seats.forEach(function(seat, index){
+            if(selectedSeats.indexOf(index) >= 0){
+                seat.classList.add("selected");
+            }
+        });
+    }
+    let selectedMovie = localStorage.getItem("selectedMovieIndex");  
+    if(!(selectedMovie == null)){
+        select.selectedIndex = selectedMovie;
+    }
 }
