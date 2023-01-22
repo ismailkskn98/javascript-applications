7a140b02d6d934d61a4959ec2eb05bfbac6546ec
İsmail Keskin <hamdismailkskn98@gmail.com>
2023-01-23 01:49:55 +0300

    hesap makinesi uygulamasının javascript kodları yapıldı. Proje tamamlandı

--- javascript-applications/hesap-makinesi/app.js
+++ javascript-applications/hesap-makinesi/app.js
@@ -7,6 +7,12 @@ let displayValue = '0';
 updateDisplay()
 // input'u sıfırlama
 
+// hesaplama için
+let firstValue = null;
+let operator = null;
+let ikinciSayiyiBekliyor = false;
+// hesaplama için
+
 //! Events
 keys.addEventListener("click", elementValues);
 
@@ -25,7 +31,9 @@ function elementValues(e) {
     //  ulaşmış olduğumuz element button ise aşağıdaki kodları çalıştır
     // button'un class'ı operator olanlar
     if (element.classList.contains('operator')) {
-        console.log(element);
+        // kullanıcıdan aldığımız operatore göre işlem yapalım
+        hangiOperator(element.value);
+        updateDisplay();
         return;
     }
     // button'un class'ı decimal olan
@@ -44,6 +52,7 @@ function elementValues(e) {
         updateDisplay();
         return;
     }
+
     // yukardakiler değilse tıklanmış olan button number'dır
     // console.log(element.value);
 
@@ -55,9 +64,19 @@ function elementValues(e) {
 
 // girdiğimiz number değeri input alanında göstermek
 function inputNumber(number) {
-    // input'un içi 0'ise bunu girilen değer ile değiştir
-    // değil ise girilen sayı veya sayıları yanına ekle
-    displayValue = displayValue === '0' ? number : displayValue + number;
+    if (ikinciSayiyiBekliyor) {
+        //inputu girilen sayı ile sıfırliyoruz (girilen sayı input'daki sayının yerini alıyor)
+        displayValue = number;
+        // ardından girdiğimiz sayı tekrar bu koşula girmemesi için "false" döndürüyoruz (ilk 5 sonra 3 = 53 gibi)
+        ikinciSayiyiBekliyor = false;
+    }
+    else {
+        // input'un içi 0'ise bunu girilen değer ile değiştir
+        // değil ise girilen sayı veya sayıları yanına ekle
+        displayValue = displayValue === '0' ? number : displayValue + number;
+    }
+    //kontrol
+    console.log("displayValue: " + displayValue, " firstValue: " + firstValue, " operator: " + operator, " ikinciSayiyiBekliyor: " + ikinciSayiyiBekliyor);
 }
 
 // decimal'e tıkladığımızda sayının sonuna nokta ekleyeceğiz
@@ -72,4 +91,50 @@ function decimal() {
 // input alanını temizleyelim
 function inputClear() {
     displayValue = '0';
+    firstValue = null;
+    ikinciSayiyiBekliyor = false;
+    operator = null;
+}
+
+// kullanıcıdan aldığımız operatore göre işlem yapalım
+function hangiOperator(girilenOperator) {
+    // input'un içindeki değer string olduğu için number'a çeviriyoruz
+    // float değer olabilirs
+    const value = parseFloat(displayValue);
+
+    // firstValue eğer null ise
+    if (firstValue == null) {
+        firstValue = value;
+    }
+    else if (operator) { // firstValue null olmadığı için (içinde ilk girilen değer var) buraya girecek
+        // operator ile işlem yapalım (eşittir)
+        const result = calculate(firstValue, value, operator);
+
+        displayValue = String(result);
+        // çıkan sonucu tekrar firstValue'nun içine yedekliyoruz ki işlem yapmaya devam edebilelim
+        firstValue = result;
+    }
+    ikinciSayiyiBekliyor = true;
+    operator = girilenOperator;
+
+    //kontrol
+    console.log("displayValue: " + displayValue, " firstValue: " + firstValue, " operator: " + operator, " ikinciSayiyiBekliyor: " + ikinciSayiyiBekliyor);
+}
+
+//operator'e göre işlem yapalım
+function calculate(first, value, operator) {
+    if (operator === '+') {
+        return first + value;
+    }
+    else if (operator === '-') {
+        return first - value;
+    }
+    else if (operator === '*') {
+        return first * value;
+    }
+    else if (operator === '/') {
+        return first / value;
+    }
+    // eşittir'e tıkladığımızda boş geri dönmemesi için (undefined) son girilen değeri input'da tutuyoruz.
+    return value;
 }
