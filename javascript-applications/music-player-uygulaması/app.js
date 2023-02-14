dbe13dc468b6abb349d67e7bf364eaa750e40e86
İsmail Keskin <hamdismailkskn98@gmail.com>
2023-02-14 18:11:32 +0300

    controls iconlarını aktif hale getirme (şarkıyı durdur başlat, bir sonraki şarkı, bir önceki şarkı)

--- javascript-applications/music-player-uygulaması/app.js
+++ javascript-applications/music-player-uygulaması/app.js
@@ -8,14 +8,10 @@ const play = document.querySelector('#controls #play');
 const next = document.querySelector('#controls #next');
 
 const player = new MusicPlayer(musicList);
-// console.log(player);
-
-player.next();
-music = player.getMusic();
+console.log(player);
 
 window.addEventListener('load', () => {
     let music = player.getMusic();
-    console.log(music);
     displayMusic(music);
 });
 
@@ -26,6 +22,50 @@ displayMusic = (music) => {
     audio.src = "mp3/" + music.file;
 }
 
+
+// play button Start
 play.addEventListener('click', () => {
-    audio.play();
+
+    const isMusicPlay = container.classList.contains('playing');
+    isMusicPlay ? musicPause() : musicPlay();
+
 });
+
+function musicPlay() {
+    container.classList.add('playing');
+    play.classList = 'fa-solid fa-pause';
+    audio.play();
+};
+function musicPause() {
+    container.classList.remove('playing');
+    play.classList = 'fa-solid fa-play';
+    audio.pause();
+};
+// play button End
+
+// prev button Start
+prev.addEventListener('click', () => {
+    musicPrev();
+});
+
+function musicPrev() {
+    player.prev();
+    let music = player.getMusic();
+    displayMusic(music);
+    let playPause = container.classList.contains('playing');
+    playPause ? musicPlay() : musicPause();
+}
+// prev button End
+
+// Next button Start
+next.addEventListener('click', () => {
+    musicNext();
+});
+
+function musicNext() {
+    player.next();
+    let music = player.getMusic();
+    displayMusic(music);
+    let playPause = container.classList.contains('playing');
+    playPause ? musicPlay() : musicPause();
+}
