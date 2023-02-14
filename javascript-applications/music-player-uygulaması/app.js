a48dde289395bc4dec3f2f0957f33756dad444af
İsmail Keskin <hamdismailkskn98@gmail.com>
2023-02-14 17:15:32 +0300

    Music bilgilerinin sayfada gösterilmesi

--- javascript-applications/music-player-uygulaması/app.js
+++ javascript-applications/music-player-uygulaması/app.js
@@ -1,27 +1,31 @@
+const container = document.querySelector('.container');
+const image = document.querySelector('#music-image');
+const audio = document.querySelector('#audio');
+const title = document.querySelector('#music-details .title');
+const singer = document.querySelector('#music-details .singer');
+const prev = document.querySelector('#controls #prev');
+const play = document.querySelector('#controls #play');
+const next = document.querySelector('#controls #next');
+
 const player = new MusicPlayer(musicList);
 // console.log(player);
-let music = player.getMusic();
-// console.log(music);
-console.log(music.getName());
+
 player.next();
 music = player.getMusic();
 
-console.log(music.getName());
-player.next();
-music = player.getMusic();
+window.addEventListener('load', () => {
+    let music = player.getMusic();
+    console.log(music);
+    displayMusic(music);
+});
 
-console.log(music.getName());
-player.next();
-music = player.getMusic();
+displayMusic = (music) => {
+    title.innerHTML = music.getName();
+    singer.innerHTML = music.singer;
+    image.src = "img/" + music.img;
+    audio.src = "mp3/" + music.file;
+}
 
-console.log(music.getName());
-player.previous();
-music = player.getMusic();
-
-console.log(music.getName());
-player.next();
-music = player.getMusic();
-
-console.log(music.getName());
-player.previous();
-music = player.getMusic();
+play.addEventListener('click', () => {
+    audio.play();
+});
